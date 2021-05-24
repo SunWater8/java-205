1a1068d89ce2492d41c22a4699f575c56934f5c4
SunWater8 <ehsoren9@gmail.com>
2021-05-24 16:01:18 +0900

    split

--- java_project/FirstJava/src/test/Member.java
+++ java_project/FirstJava/src/test/Member.java
@@ -1,18 +1,5 @@
 package test;
-class Calculator{
-	int add(int a, int b) {
-		return a+b;
-	}
-	int subtract(int a, int b) {
-		return a-b;
-	}
-	int multiply(int a, int b) {
-		return a*b;
-	}
-	int divide(int a, int b) {
-		return a/b;
-	}
-}
+
 
 public class Member {
 	public static void main(String[] args) {
