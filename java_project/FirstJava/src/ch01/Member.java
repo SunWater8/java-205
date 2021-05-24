8438576c30c7aa862a74287f9df75a0feac5f093
SunWater8 <ehsoren9@gmail.com>
2021-05-24 18:13:37 +0900

    edit

--- java_project/FirstJava/src/ch01/Member.java
+++ java_project/FirstJava/src/ch01/Member.java
@@ -3,16 +3,5 @@ package ch01;
 public class Member {
 	public static void main(String[] args) {
 		System.out.println("김은선 입니다.");
-		
-		String name = "Jamie";
-		int age = 10;
-		double height = 130.5;
-		boolean hasBook = true;
-		
-		System.out.println(name);
-		System.out.println(age);
-		System.out.println(height);
-		System.out.println(hasBook);
-		
 	}
 }
