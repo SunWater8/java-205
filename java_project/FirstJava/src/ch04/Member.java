95599a44b1b0cf924b3c6c9e4384be14d82afd11
silversoren <ehsoren9@gmail.com>
2021-05-25 15:44:38 +0900

    except previous

--- java_project/FirstJava/src/ch04/Member.java
+++ java_project/FirstJava/src/ch04/Member.java
@@ -26,14 +26,14 @@ public class Member {
 		}
 	}
 	public static void main(String[] args) {
-		String name = "Jamie";
-		int age = 10;
-		double height = 130.5;
-		boolean hasBook = true;
-		System.out.println("My name is "+name);
-		System.out.println("She is "+age+"years old");
-		System.out.println("Her height is "+height);
-		System.out.println("Having a Java Book : "+ hasBook);
+//		String name = "Jamie";
+//		int age = 10;
+//		double height = 130.5;
+//		boolean hasBook = true;
+//		System.out.println("My name is "+name);
+//		System.out.println("She is "+age+"years old");
+//		System.out.println("Her height is "+height);
+//		System.out.println("Having a Java Book : "+ hasBook);
 
 		Member m = new Member();
 		
