e17b1cb9081a8306e24809bb424cb191c6ad3456
SunWater8 <ehsoren9@gmail.com>
2021-05-26 19:38:12 +0900

    sum & avg of korScore

--- java_project/FirstJava/src/ch06/ScoreManager.java
+++ java_project/FirstJava/src/ch06/ScoreManager.java
@@ -3,32 +3,27 @@ package ch06;
 public class ScoreManager {
 
 	public static void main(String[] args) {
-		int[] korTest = {80,86,94,75,85,64,54,24,78,55};
-		int[] engTest = {87,85,94,65,45,75,53,61,82,94};
-		int[] mathTest = {75,94,86,57,64,82,93,81,72,68};
 		
-		int sum1=0;
-		for(int idx = 0; idx<korTest.length;idx++) {
-			System.out.println(korTest[idx]);
-			sum1 += korTest[idx];
+		int[] korScore = new int[10]; 
+		
+		korScore[0]=90;
+		korScore[1]=56;
+		korScore[2]=97;
+		korScore[3]=97;
+		korScore[4]=75;
+		korScore[5]=68;
+		korScore[6]=79;
+		korScore[7]=62;
+		korScore[8]=91;
+		korScore[9]=83;
+		
+		int sum=0;
+		for(int i=0; i<korScore.length;i++) {
+			System.out.println(korScore[i]);
+			sum += korScore[i];
 		}
-		
-		int sum2=0;
-		for(int idx = 0; idx<engTest.length;idx++) {
-			System.out.println(engTest[idx]);
-			sum2 += engTest[idx];
-		}
-		
-		int sum3 = 0;
-		for(int i = 0; i<mathTest.length;i++) {
-			System.out.println(mathTest[i]);
-			sum3 += mathTest[i];
-		}
-		float avg1 = (float)sum1/korTest.length;
-		float avg2 = (float)sum2/engTest.length;
-		float avg3 = (float)sum3/mathTest.length;
-		
-		
+		System.out.println("총합 : "+sum);
+		System.out.println("평균 : "+(float)sum/korScore.length);
 		
 	}
 
