aa8c21df4f8392f7e48356fe71a0bd22aeb01bd5
silversoren <ehsoren9@gmail.com>
2021-05-31 21:46:33 +0900

    변경된 메소드 명 수정

--- java_project/FirstJava/src/ch05/MemberMain.java
+++ java_project/FirstJava/src/ch05/MemberMain.java
@@ -5,7 +5,7 @@ public class MemberMain {
 	public static void main(String[] args) {
 		Member member = new Member("Jamie", "010-5989-1245", "불어",1,"abcd@naver.com", "1900년 0월 0일", "서울시 마포구");
 		member.setName("영희");
-		member.print();
+		member.showInfo();
 		
 	}
 
