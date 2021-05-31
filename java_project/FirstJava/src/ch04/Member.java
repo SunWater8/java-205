cc865e025304455c596c136e1e005041f3cba1f3
silversoren <ehsoren9@gmail.com>
2021-05-31 21:39:44 +0900

    조건식 논리 수정

--- java_project/FirstJava/src/ch04/Member.java
+++ java_project/FirstJava/src/ch04/Member.java
@@ -1,49 +1,61 @@
 package ch04;
+
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class Member {
-	
-	int thisYear = 2021;
-	
+
+	// 태어난 년도를 입력하면 나이를 반환하는 메소드
+	int ageCal(int year) {
+		return Calendar.getInstance().get(Calendar.YEAR) - year - 1;// 만나이 계산
+	}
+
 	void vaccine (int birthYear) {
 		int age = ageCal(birthYear);
-		if(!(15<=age && 65>age) ){
+		
+		boolean check1 = age < 15 || age >= 65;
+		boolean check2 = !(age >= 15 && age < 65);
+		
+		System.out.println(check1);
+		System.out.println(check2);
+		
+		if(check2) {
 			System.out.println("무료예방접종이 가능합니다.");
 		}else {
 			System.out.println("무료접종 대상이 아닙니다.");
 		}
 	}
 
-	int ageCal(int year) {
-		return Calendar.getInstance().get(Calendar.YEAR)-year-1;//만나이 계산
-	}
-
 	void exam(int birthYear) {
 		int age = ageCal(birthYear);
-		if((20<=age) && (age%2==0)) {
+
+		boolean check1 = birthYear % 2 == 0 && 2021 % 2 == 0 || birthYear % 2 == 1 && 2021 % 2 == 1;
+		boolean check2 = birthYear % 2 == 2021 % 2;
+
+		if ((20 <= age) && check2) {
 			System.out.println("당신은 올 해 건강검진 대상입니다.");
-			if(40<=age) {
+			if (40 <= age) {
 				System.out.println("그리고 암 검사는 무료입니다.");
 			}
-		}else {
+		} else {
 			System.out.println("올 해 건강검진 대상자가 아닙니다.");
 		}
 	}
-	public static void main(String[] args) {
 
-		Member m = new Member();
+	public static void main(String[] args) {
+		System.out.println(Calendar.getInstance().get(Calendar.YEAR));
 		
-		System.out.println();
-		Scanner scanner=new Scanner(System.in);
+		Member m = new Member();
+
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("이름을 입력해 주세요 >");
 		String name1 = scanner.nextLine();
 		System.out.println("태어난 년도를 입력해 주세요 >");
 		int birthYear = scanner.nextInt();
-		
-		System.out.println(name1+" 님은 ");
+
+		System.out.println(name1 + " 님은 ");
 		m.vaccine(birthYear);
 		m.exam(birthYear);
-		
+
 	}
 }
