462cb91339ac0fc7185e1d0ed5f4444f8a878c02
silversoren <ehsoren9@gmail.com>
2021-05-31 21:54:02 +0900

    생성자 앞에 public 추가 입력

--- java_project/FirstJava/src/ch06/Student.java
+++ java_project/FirstJava/src/ch06/Student.java
@@ -1,50 +1,60 @@
 package ch06;
 
 public class Student {
-	
+
 	private String name;
 	private int korTest;
 	private int engTest;
 	private int mathTest;
-	
-	public void setName(String name) {
-		this.name=name;
+
+	public Student() {
 	}
+
+	public Student(String name, int korTest, int engTest, int mathTest) {
+		this.name = name;
+		this.korTest = korTest;
+		this.engTest = engTest;
+		this.mathTest = mathTest;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setKorTest(int korTest) {
-		this.korTest=korTest;
+		this.korTest = korTest;
 	}
+
 	public int getKorTest() {
 		return korTest;
 	}
+
 	public void setEngTest(int engTest) {
-		this.engTest=engTest;
+		this.engTest = engTest;
 	}
+
 	public int getEngTest() {
 		return engTest;
 	}
+
 	public void setMathTest(int mathTest) {
-		this.mathTest=mathTest;
+		this.mathTest = mathTest;
 	}
+
 	public int getMathTest() {
 		return mathTest;
 	}
-	Student(){}
-	
-	Student(String name, int korTest, int engTest, int mathTest){
-		this.name=name;
-		this.korTest=korTest;
-		this.engTest=engTest;
-		this.mathTest=mathTest;
+
+	int total() {
+		return this.korTest + this.engTest + this.mathTest;
 	}
-	
-	public int total() {
-		return this.korTest+this.engTest+this.mathTest;
+
+	float avg() {
+		return total() / 3f;
 	}
-	public float avg() {
-		return total()/3f;
-	}
-		
+
 }
