086c39ec0ded2eed8aef814ca4bc377f260cb1ad
SunWater8 <ehsoren9@gmail.com>
2021-06-01 10:09:02 +0900

    Integer타입의 hashSet

---
+++ java_project/FirstJava/src/ex/CollectionFramework_0601/HashSet2.java
@@ -0,0 +1,25 @@
+//Integer 타입의 hashSet
+
+package ex.CollectionFramework_0601;
+import java.util.*;
+
+public class HashSet2 {
+	public static void main(String[] args) {
+
+		HashSet<Integer> hashSet = new HashSet<Integer>(); 
+
+		// 데이터 저장
+		hashSet.add(1);
+		hashSet.add(2);
+		hashSet.add(1);
+		hashSet.add(2);
+		hashSet.add(3);
+
+		// 데이터의 일괄 처리
+		Iterator<Integer> itr = hashSet.iterator();
+
+		while (itr.hasNext()) { 
+			System.out.println(itr.next());
+		}
+	}
+}
