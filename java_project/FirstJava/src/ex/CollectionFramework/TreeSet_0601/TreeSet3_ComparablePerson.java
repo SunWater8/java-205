9e7f5b7100ef4f024ab50c94d71aa684e6c5d4d4
SunWater8 <ehsoren9@gmail.com>
2021-06-01 11:40:45 +0900

    Update TreeSet3_ComparablePerson.java

--- java_project/FirstJava/src/ex/CollectionFramework/TreeSet_0601/TreeSet3_ComparablePerson.java
+++ java_project/FirstJava/src/ex/CollectionFramework/TreeSet_0601/TreeSet3_ComparablePerson.java
@@ -2,9 +2,16 @@ package ex.CollectionFramework.TreeSet_0601;
 import java.util.*;
 public class TreeSet3_ComparablePerson {
 	public static void main(String[] args) {
-		Person p = new Person("jamie",20);
-		p.showData();
-		p.compareTo(p);
+		TreeSet<Person> set = new TreeSet<>();
+		set.add(new Person("jamie",20));
+		set.add(new Person("may",25));
+		set.add(new Person("paul",30));
+		
+		//나이로 정렬하기
+		Iterator<Person> itr = set.iterator();
+		while(itr.hasNext()) {
+			itr.next().showData();
+		}
 	}
 }
 
@@ -22,17 +29,20 @@ class Person implements Comparable<Person>{
 
 	@Override
 	public int compareTo(Person o) {
-		if(this.age>o.age) {
-			return 10; //양수 아무 숫자 return 
-		}else if(this.age<o.age) {
-			return -10; //음수 아무 숫자 return
-		}else {
-			return 0;
-		}
 		
+		//정렬하는 4가지 방법의 코드
 		
-//		return this.age-o.age;
+		//오름차순으로 정렬1
+//		if(this.age>o.age) {
+//			return 10; //양수 아무 숫자 return 
+//		}else if(this.age<o.age) {
+//			return -10; //음수 아무 숫자 return
+//		}else {
+//			return 0;
+//		}
 		
-//		return this.age-o.age*-1;//반대 정렬 하고 싶을 때.
+		return this.age-o.age; //오름차순으로 정렬2.
+//		return o.age - this.age; // 내림차순으로 정렬1
+//		return this.age-o.age*-1;//내림차순으로 정렬2.
 	}
 }
