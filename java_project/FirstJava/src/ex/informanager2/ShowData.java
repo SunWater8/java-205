d27529316e474736aebda8cbb3a99b3013292186
silversoren <ehsoren9@gmail.com>
2021-06-13 19:02:28 +0900

    delete

--- java_project/FirstJava/src/ex/informanager2/ShowData.java
+++
@@ -1,6 +0,0 @@
-package ex.informanager2;
-
-public interface ShowData {
-	//인스턴스의 기본 정보를 출력하는 메소드로 정의하세요.
-	void showBasicInfo();//Friend클래스에서 가져옴. public abstract 생략
-}
---
+++ java_project/JavaEx/src/ppt08_Array/AccessArray.java
@@ -0,0 +1,10 @@
+package ppt08_Array;
+
+public class AccessArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
