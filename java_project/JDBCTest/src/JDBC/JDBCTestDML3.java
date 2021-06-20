3c1aa91a4de1098336c62bd91f15279d67c5aee1
silversoren <ehsoren9@gmail.com>
2021-06-20 13:13:54 +0900

    DML - update

--- java_project/JDBCTest/src/JDBC/JDBCTestDML3.java
+++ java_project/JDBCTest/src/JDBC/JDBCTestDML3.java
@@ -6,7 +6,7 @@ public class JDBCTestDML3 {
 
 	public static void main(String[] args) {
 		//finally 에서 처리되기 위해 이 곳에 변수를 선언한다.
-		
+
 		//연결 객체 : 연결 정보를 가진다.
 		Connection conn = null;
 		//sql을 실행할 메소드 제공
@@ -15,56 +15,62 @@ public class JDBCTestDML3 {
 		ResultSet rs= null;
 		//statement -> PreparedStatement : 성능 개선.
 		PreparedStatement pstmt = null;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		try {
 			//1. 드라이버 로드
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("드라이버 로드 성공!!");
-			
+
 			//2. 연결
 			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
 			String user = "hr";
 			String pw = "tiger";
-			
+
 			conn = DriverManager.getConnection(jdbcUrl, user, pw);
 			System.out.println("데이터 베이스 연결 성공!!");
-			
+
 			//트렌젝션 설정
 			conn.setAutoCommit(false);
-			
-			
-			//3. sql 처리
-			//사용자에게 정보를 받아 부서 데이터를 입력하는 프로그램을 만들어보자
-			System.out.println("\n부서 정보 입력을 시작합니다.");
-			System.out.println("부서 이름을 입력해 주세요.");
-			String dname = sc.nextLine();
-			System.out.println("부서 위치를 입력해 주세요.");
-			String loc = sc.nextLine();
 
-			String sql = "insert into dept01 values (DEPT01_DEPTNO_SEQ.nextVal, ?, ?)";
+			//3. sql 작성
+			// 사용자에게 정보를 받아 데이터를 수정
+			// 10 dev seoul ==> " " 
+			System.out.println("부서 데이터의 수정을 시작합니다.");
+			System.out.println("10 dev seoul 형식으로 데이터를 입력해 주세요.");
+			String input = sc.nextLine();
+
+			// String[]
+			String[] inputs = input.split(" ");
+
+			//						for(String str : inputs) {
+			//							System.out.println(str);
+			//						}
+
+			String sql = "update dept01 set dname=?, loc=? where deptno=?";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, dname);
-			pstmt.setString(2, loc);
-			
-			int result = pstmt.executeUpdate(); //executeUpdate() 메소드는 반영된 로우의 개수를 반환한다.
-			
-			if(result >0) { 
-				System.out.println("입력되었습니다.");
-			}else {
-				 System.out.println("입력 실패!!!");
+			pstmt.setString(1,inputs[1]);
+			pstmt.setString(2, inputs[2]);
+			pstmt.setInt(3, Integer.parseInt(inputs[0]));
+
+			int result = pstmt.executeUpdate();
+
+			if(result > 0) {
+				System.out.println("수정되었습니다.");
+			} else {
+				System.out.println("찾으시는 부서가 존재하지 않습니다.");
 			}
-			
+
 			//트랜젝션의 완료(성공)
 			conn.commit();
-			
+
 		} catch (ClassNotFoundException e) { //위의 주소 문장에서 내가 잘못 입력했을 경우 오류가 발생할 수 있다.
 			System.out.println("드라이버 클래스를 찾지 못함!!!");
 			e.printStackTrace();
 		} catch (SQLException e) {
-//			System.out.println("데이터 베이스 연결 실패!!");
-			
+			//System.out.println("데이터 베이스 연결 실패!!");
+
 			//트랜젝션을 rollback하게 될 때 예외
 			try {
 				conn.rollback();
@@ -72,11 +78,11 @@ public class JDBCTestDML3 {
 				e1.printStackTrace();
 			}
 			e.printStackTrace();
-		
+
 		}finally {
-						
+
 			//4. close
-			
+
 			if(rs != null) {
 				try {
 					rs.close();
@@ -95,12 +101,12 @@ public class JDBCTestDML3 {
 				try {
 					conn.close();
 				} catch (SQLException e) {
-					
+
 					e.printStackTrace();
 				}
 			}
 		}
-		
+
 	}
 
 }
