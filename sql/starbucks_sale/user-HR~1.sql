7b69e1c83409efb0c2d26b8a269f79dad4e4b853
silversoren <ehsoren9@gmail.com>
2021-06-23 11:09:50 +0900

    Create user-HR~1.sql

---
+++ sql/starbucks_sale/user-HR~1.sql
@@ -0,0 +1,22 @@
+--sale table의 데이터
+
+insert into sale(salecode, sname, price,saledate)
+values (sale_salecode_seq.nextval, 'latte', 4600, '21/06/21');
+
+insert into sale(salecode, sname, price,saledate)
+values (sale_salecode_seq.nextval, 'salad', 5000, '21/06/21');
+
+insert into sale(salecode, sname, price,saledate)
+values (sale_salecode_seq.nextval, 'cake', 5500, '21/06/22');
+insert into sale(salecode, sname, price,saledate)
+values (sale_salecode_seq.nextval, 'Amerciano', 4100, '21/06/22');
+
+insert into sale (salecode, sname, price)
+values(sale_salecode_seq.nextval, 'americano', 4100);
+
+insert into sale(salecode, sname, price)
+values (sale_salecode_seq.nextval, 'latte', 4600);
+
+insert into sale(salecode, sname, price)
+values (sale_salecode_seq.nextval, 'sandwich', 6200);
+
