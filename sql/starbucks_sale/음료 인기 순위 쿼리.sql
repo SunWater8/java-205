d765b40e9aaa10664ef18f203519b8933a9eaebb
silversoren <ehsoren9@gmail.com>
2021-06-24 12:41:04 +0900

    Create 음료 인기 순위 쿼리.sql

---
+++ sql/starbucks_sale/음료 인기 순위 쿼리.sql
@@ -0,0 +1,16 @@
+-- 팔리는 수량 인기 순위
+select distinct sname, count(sname) as count
+from sale
+group by sname
+order by count desc 
+;
+
+-- rownum 활용하여 top 3 출력
+select rownum, sname
+from (select distinct sname, count(sname) as count
+from sale
+group by sname
+order by count desc )
+where rownum < 4;
+
+--문제?? 동일한 개수일 때 
