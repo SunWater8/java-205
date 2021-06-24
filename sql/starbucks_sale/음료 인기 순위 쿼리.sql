ff4a341e43024f5ff931f58c1501f44f6455a99f
silversoren <ehsoren9@gmail.com>
2021-06-24 18:14:36 +0900

    delete

--- sql/starbucks_sale/음료 인기 순위 쿼리.sql
+++ sql/starbucks_sale/음료 인기 순위 쿼리.sql
@@ -6,7 +6,7 @@ order by count desc
 ;
 
 -- rownum 활용하여 top 3 출력
-select rownum, sname
+select rownum, sname, count
 from (select distinct sname, count(sname) as count
 from sale
 group by sname
@@ -14,3 +14,5 @@ order by count desc )
 where rownum < 4;
 
 --문제?? 동일한 개수일 때 
+
+select * from sale;
