861a6090fa3fbd85d690452e234b66c4454ad435
silversoren <ehsoren9@gmail.com>
2021-08-17 00:18:42 +0900

    mybatis 추가

--- spring_project1/OpenProject_connectDB/src/main/java/com/bitcamp/op/member/controller/MemberRegController.java
+++ spring_project1/OpenProject_connectDB/src/main/java/com/bitcamp/op/member/controller/MemberRegController.java
@@ -9,36 +9,43 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.bitcamp.op.member.domain.RegRequest;
+import com.bitcamp.op.member.domain.MemberRegRequest;
 import com.bitcamp.op.member.service.MemberRegService;
 
 @Controller
 @RequestMapping("/member/memberReg")
 public class MemberRegController {
-
+	
 	@Autowired
 	private MemberRegService regService;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String regForm() {
-		return "/member/regForm";
+		return "member/regForm";
 	}
 	
-	@RequestMapping (method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public String reg(
-			
-			@ModelAttribute("regRequest") RegRequest regRequest,
+			@ModelAttribute("regRequest") MemberRegRequest regRequest,
 			HttpServletRequest request,
 			Model model
-			) { 
+			) {
+		//System.out.println(regRequest);
 		
-		boolean result =  regService.memberReg(regRequest, request);
+		int result = regService.memberReg(regRequest, request);
 		
 		model.addAttribute("result", result);
-		String view = "member/memberReg";
-		if(result) {
-			view="redirect:/index";
+		
+		String view = "member/reg" ;
+		if(result == 1) {
+			// 인텍스 페이지로 리다이렉트
+			view = "redirect:/index";
 		}
 		return view;
 	}
-} 
+	
+}
+
+
+
+
