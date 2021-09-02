ca5d88fb298757cabda664094561d803121f9b66
silversoren <ehsoren9@gmail.com>
2021-09-02 14:55:27 +0900

    0901 demoApp for Spring boot

--- spring_project1/DemoApp/src/main/java/com/bitcamp/app/DemoAppApplication.java
+++ spring_project1/DemoApp/src/main/java/com/bitcamp/app/DemoAppApplication.java
@@ -1,9 +1,13 @@
 package com.bitcamp.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//mapper를 괄호 안의 패키지 에서 찾을 수 있게 하는 기능
+@MapperScan("com.bitcamp.app.member.mapper")
 public class DemoAppApplication {
 
 	public static void main(String[] args) {
--- spring_project1/DemoApp/src/test/java/com/bitcamp/app/DemoAppApplicationTests.java
+++ spring_project1/DemoApp/src/test/java/com/bitcamp/app/DemoAppApplicationTests.java
@@ -1,13 +1,32 @@
 package com.bitcamp.app;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoAppApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+	
+	private Connection conn;
+	
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testConnection() throws SQLException {
+		//별도의 xml 설정 없이도 구동이 된다는 것을 확인하기 위해 작성.
+		System.out.println("dataSource >>>>>>>>"+dataSource);
+		
+		conn = dataSource.getConnection();
+		System.out.println("connection >>>>>>>>"+conn);
+	}
 }
